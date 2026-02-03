ff9ada4bad6492c921298d5720c206164c361324
jwpark0215 <jwpark021523@outlook.com>
2026-02-03 18:07:06 +1100

    added api and cs to the application

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,16 @@
 const API_KEY = ee960c8a3dda51cf7ce915e03edaf9a8
 const BASE_URL = 'https://api.themoviedb.org/3'
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`) 
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`) 
+    const data = await response.json()
+    return data.results
+}
+
+
